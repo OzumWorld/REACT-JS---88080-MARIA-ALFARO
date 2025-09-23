d853719bfae1f3ff05b5bd4ffd7d76b86558a0bf
admin <mariaalfaro@ozumworld.com>
2025-09-22 23:10:44 -0300

    Vite base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/REACT-JS---88080-MARIA-ALFARO/', // 👈 importante para GitHub Pages
 })
